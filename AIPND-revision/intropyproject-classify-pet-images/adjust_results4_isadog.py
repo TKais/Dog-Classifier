b6e99c53141eb6c188f4c19969b3e4c20bb44335
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2019-02-16 22:02:03 -0800

    feat: Add dev name

--- AIPND-revision/intropyproject-classify-pet-images/adjust_results4_isadog.py
+++ AIPND-revision/intropyproject-classify-pet-images/adjust_results4_isadog.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/adjust_results4_isadog.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                 
+# PROGRAMMER: Tiffany Kaiser
+# DATE CREATED: 2/16/2018                    
 # REVISED DATE: 
 # PURPOSE: Create a function adjust_results4_isadog that adjusts the results 
 #          dictionary to indicate whether or not the pet image label is of-a-dog, 
